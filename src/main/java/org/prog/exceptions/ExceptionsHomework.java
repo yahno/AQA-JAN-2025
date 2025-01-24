afed742d0faf407a522f3f490dd706b39984aa2c
yahno <olgayahno@gmail.com>
2025-01-24 17:08:05 +0200

    homework3_1(NPE)

--- src/main/java/org/prog/exceptions/ExceptionsHomework.java
+++ src/main/java/org/prog/exceptions/ExceptionsHomework.java
@@ -1,9 +1,17 @@
 package org.prog.exceptions;
 
+import org.prog.parent.Car;
 
-//TODO: write a method which ca cause NullPointerException
-// TODO: write try-catc-finally to catch NPE
+// TODO: write a method which ca cause NullPointerException
+// TODO: write try-catch-finally to catch NPE
 // TODO: on exception -> print "EXCEPTION CAUGHT!"
 // TODO: always print "DONE TRY-CATCH-FINALLY"
 public class ExceptionsHomework {
+    public static void main(String[] args) {
+        Car carOne = new Car("red");
+        Car carTwo = new Car("blue");
+        Car carThree = new Car(null);
+
+        System.out.println(carThree.color.length());
+    }
 }
