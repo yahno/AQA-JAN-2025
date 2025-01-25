eb37f751e25b12c9b7cef712ed87dacd5964d6ca
yahno <olgayahno@gmail.com>
2025-01-25 13:16:14 +0200

    homework3_11(NPE)

--- src/main/java/org/prog/exceptions/ExceptionsHomework.java
+++ src/main/java/org/prog/exceptions/ExceptionsHomework.java
@@ -2,16 +2,26 @@ package org.prog.exceptions;
 
 import org.prog.parent.Car;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 // TODO: write a method which ca cause NullPointerException
 // TODO: write try-catch-finally to catch NPE
 // TODO: on exception -> print "EXCEPTION CAUGHT!"
 // TODO: always print "DONE TRY-CATCH-FINALLY"
 public class ExceptionsHomework {
     public static void main(String[] args) {
-        Car carOne = new Car("red");
-        Car carTwo = new Car("blue");
-        Car carThree = new Car(null);
-
-        System.out.println(carThree.color.length());
+        //Car carOne = new Car("red");
+        //Car carTwo = new Car("blue");
+        //Car carThree = new Car(null);
+        //System.out.println(carThree.color.length());
+        //smth1(s:null);
+        smth(null);
     }
+
+    public static void smth(String s) {
+        System.out.println(s.equals("a"));
+    }
+
 }
