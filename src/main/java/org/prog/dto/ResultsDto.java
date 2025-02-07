c80fbe0664a17ed9bfefecd059d76d0b4b2dbc81
aosipenko <aoskiev@gmail.com>
2025-02-07 20:00:34 +0100

    Session 8: RestAssured

---
+++ src/main/java/org/prog/dto/ResultsDto.java
@@ -0,0 +1,15 @@
+package org.prog.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ResultsDto {
+
+    public List<PersonDto> results;
+}
---
+++ src/test/java/org/prog/rest/ApiTests.java
@@ -0,0 +1,37 @@
+package org.prog.rest;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.prog.dto.ResultsDto;
+import org.testng.annotations.Test;
+
+public class ApiTests {
+
+    @Test
+    public void apiTest() {
+        RequestSpecification requestSpecification = RestAssured.given();
+        requestSpecification.baseUri("https://randomuser.me/");
+        requestSpecification.basePath("/api");
+        requestSpecification.queryParam("inc", "gender,name,nat");
+        requestSpecification.queryParam("noinfo");
+        requestSpecification.queryParam("results", "30");
+
+        Response response = requestSpecification.get();
+
+        ResultsDto results = response.as(ResultsDto.class);
+        System.out.println(results.getResults().get(0).getName().getFirst());
+        System.out.println(results.getResults().get(0).getName().getLast());
+//        String value = response.jsonPath().get("results[0].gender");
+//        System.out.println(value);
+//        Assert.assertEquals(value, "female");
+//        response.prettyPrint();
+//
+//        ValidatableResponse validatableResponse = response.then();
+//        validatableResponse.statusCode(200);
+//        validatableResponse.contentType(ContentType.JSON);
+//        validatableResponse.body("results[0].gender", equalTo("male"));
+
+
+    }
+}
