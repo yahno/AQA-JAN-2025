b01a8d3cbed8ef8b9d5d0250683c894338bb267f
aosipenko <aoskiev@gmail.com>
2025-02-24 19:23:28 +0100

    Session 12: Web Driver Factory

--- src/test/java/org/prog/cucumber/CucumberRunner.java
+++ src/test/java/org/prog/cucumber/CucumberRunner.java
@@ -3,17 +3,14 @@ package org.prog.cucumber;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.remote.RemoteWebDriver;
 import org.prog.cucumber.steps.SqlSteps;
 import org.prog.cucumber.steps.WebSteps;
-import org.prog.pages.GooglePage;
 import org.prog.pages.W3SchoolsPage;
+import org.prog.util.WebDriverFactory;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 
 import java.net.MalformedURLException;
-import java.net.URL;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
@@ -31,11 +28,17 @@ public class CucumberRunner extends AbstractTestNGCucumberTests {
 
     @BeforeSuite
     public void setUp() throws ClassNotFoundException, SQLException, MalformedURLException {
-        this.driver = getRemoteDriver();
+        String envType = System.getProperty("envType", "jenkins");
+        this.driver = WebDriverFactory.getDriver(envType);
         WebSteps.w3SchoolsPage = new W3SchoolsPage(driver);
         Class.forName("com.mysql.cj.jdbc.Driver");
-        SqlSteps.connection =
-                DriverManager.getConnection("jdbc:mysql://mysql-db-1:3306/db", "user", "password");
+        if ("jenkins".equals(envType)) {
+            SqlSteps.connection =
+                    DriverManager.getConnection("jdbc:mysql://mysql-db-1:3306/db", "user", "password");
+        } else {
+            SqlSteps.connection =
+                    DriverManager.getConnection("jdbc:mysql://localhost:3306/db", "user", "password");
+        }
     }
 
     @AfterSuite
@@ -44,10 +47,5 @@ public class CucumberRunner extends AbstractTestNGCucumberTests {
         if (driver != null) {
             driver.quit();
         }
-    }
-
-    public WebDriver getRemoteDriver() throws MalformedURLException {
-        return new RemoteWebDriver(
-                new URL("http://selenoid-selenoid-1:4444/wd/hub"), new ChromeOptions());
     }
 }
